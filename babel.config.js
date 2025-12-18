4ae16c6e321a6cf5fa90c02c5aa34b24f73c3fdb
Femi Kolade <koladefemi@gmail.com>
2025-12-18 07:23:41 -0600

    clean up babel

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,6 @@ module.exports = function (api) {
 
   var unistylesPluginOptions = {
     root: "packages/ui",
-    autoProcessRoot: "src",
     debug: babelEnv !== "production",
     autoProcessImports: ["@example/theme", "@example/ui"],
   };
